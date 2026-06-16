cba89c0a0bde73678298587c8f7592efe553d2d5
Kakashi7860 <sohelshakh91@gmail.com>
2026-06-16 20:21:48 +0530

    first commit

---
+++ src/com/sohail/Main.java
@@ -0,0 +1,16 @@
+package com.sohail;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {38, 27, 43, 3, 9, 82, 10};
+        
+        // Sort the entire array
+        Arrays.sort(numbers); 
+        System.out.println(Arrays.toString(numbers)); // Output: [1, 2, 3, 5, 9]
+
+       
+    }
+}
+
